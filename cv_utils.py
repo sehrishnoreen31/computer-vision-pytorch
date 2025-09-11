3908e78ca62dcb7d598cc96f93da36daf335733b
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-09-11 12:11:48 +0500

    cv utility functions, activations

---
+++ cv_utils.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def tanh(x):
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+def relu(x):
+    return(x > 0, x , 0) # if the element is greater than 0, return that element, Otherwise, return 0.
+
+def linear(x):
+    return x
